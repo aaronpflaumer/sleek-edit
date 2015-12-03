dbf906e7e4b8738dfbb67b84e7d430e15c521ba4
Aaron Pflaumer <teahermit@gmail.com>
2015-12-03 15:39:03 -0800

    Yeoman Angular scaffolding.

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sleekEditApp
+ * @description
+ * # sleekEditApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sleekEditApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sleekEditApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sleekEditApp
+ */
+angular.module('sleekEditApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
