388b48e9ce1b9193bb04896039ea738351f93358
Aaron Pflaumer <teahermit@gmail.com>
2015-12-10 15:09:19 -0800

    Install angular-local-storage.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function(config) {
       'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
       'bower_components/oauthio-web/dist/oauth.min.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/angular-local-storage/dist/angular-local-storage.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
