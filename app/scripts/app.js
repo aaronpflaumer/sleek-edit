cd58aa4bc82abc91fa723a7388d48eee9fa91828
Aaron Pflaumer <teahermit@gmail.com>
2016-01-22 13:14:40 -0800

    State buttons and remove test code.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,30 +22,12 @@ angular
         url: "/",
         templateUrl: "../views/main.html",
         controller: 'MainCtrl'
-        // resolve: { authenticate: authenticate }
       })
       .state('login', {
         url: "/login",
         templateUrl: "../views/login.html",
         controller: 'LoginCtrl'
       });
-
-      // function authenticate($q, localstorage, $state, $timeout) {
-      //   if (user.isAuthenticated()) {
-      //     // Resolve the promise successfully
-      //     return $q.when();
-      //   } else {
-      //
-      //     $timeout(function() {
-      //       // This code runs after the authentication promise has been rejected.
-      //       // Go to the log-in page
-      //       $state.go('login');
-      //     });
-      //
-      //     // Reject the authentication promise to prevent the state from loading
-      //     return $q.reject();
-      //   }
-      // }
 
       localStorageServiceProvider
         .setPrefix('sleekEdit');
